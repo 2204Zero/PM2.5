d65b0906a328b5955d00a87f5c1332524e716b4d
lakshya goyal (zero) <lakshyagoyal701@gmail.com>
2026-03-03 14:07:12 +0530

    Fix login to use axios instance with correct baseURL

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "../axiosConfig";
+import instance from "../axiosConfig";
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -19,7 +19,7 @@ function Login() {
       formData.append("username", username);
       formData.append("password", password);
 
-      const response = await axios.post(
+      const response = await instance.post(
         "/auth/login",
         formData,
         {
